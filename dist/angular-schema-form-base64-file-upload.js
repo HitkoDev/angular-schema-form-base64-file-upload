8b2a19b71f7271341f618c026c614c7690a7cfbb
Marko Novak <marko.novak@erpium.com>
2016-11-23 14:23:07 +0100

    Version 1.2.0

--- dist/angular-schema-form-base64-file-upload.js
+++ dist/angular-schema-form-base64-file-upload.js
@@ -1,185 +1,244 @@
-angular.module("templates", []).run(["$templateCache", function($templateCache) {$templateCache.put("src/templates/angular-schema-form-base64-file-upload.html","<div class=\"angular-schema-form--base64-file\" ng-class=\"{\'has-error\': form.disableErrorState !== true && hasError(), \'has-success\': form.disableSuccessState !== true && hasSuccess(), \'has-feedback\': form.feedback !== false}\">\n  <label class=\"control-label\" ng-hide=\"form.notitle\">{{form.title}}</label>\n  <div base64-file-upload=\"form\" sf-field-model schema-validate=\"form\">\n    <input type=\"file\" class=\"base64-file--input\" style=\"visibility:hidden;position:absolute;top:0;left:0;\" ng-disabled=\"hasFile\"/>\n    <button type=\"button\" class=\"base64-file--drop-area\" ng-class=\"{\'file-hovering\': dropAreaHover, \'has-file\': hasFile}\">\n\n      <div class=\"base64-file--file\" ng-show=\"hasFile\">\n        <div class=\"base64-file--file-preview\"\n             ng-style=\"{\'background-image\': isImage(file) ? \'url(\' + file.src + \')\': \'\'}\">\n          <span ng-show=\"!isImage(file)\">{{file.ext}}</span>\n        </div>\n        <div class=\"base64-file--file-name\">{{file.name}}</div>\n        <div class=\"base64-file--file-size\">{{file.humanSize}}</div>\n        <div class=\"base64-file--file-remove\" ng-click=\"removeFile($event)\">&#10005</div>\n      </div>\n      <span ng-hide=\"hasFile\" class=\"base64-file--drop-area-description\">{{form.placeholder || dropText}}</span>\n    </button>\n  </div>\n\n  <span class=\"help-block\" sf-message=\"form.description\"></span>\n</div>\n");}]);
+angular.module("templates", []).run(["$templateCache", function($templateCache) {$templateCache.put("src/templates/angular-schema-form-base64-file-upload.html","<div class=\"angular-schema-form--base64-file\" ng-class=\"{\'has-error\': form.disableErrorState !== true && hasError(), \'has-success\': form.disableSuccessState !== true && hasSuccess(), \'has-feedback\': form.feedback !== false}\"><label class=\"control-label\" ng-hide=\"form.notitle\">{{form.title}}</label><div base64-file-upload=\"form\" sf-field-model=\"\" schema-validate=\"form\" class=\"base64-file--drop-area\" ng-class=\"{\'file-hovering\': dropAreaHover, \'has-file\': hasFile}\"><input type=\"file\" class=\"base64-file--input\" style=\"visibility:hidden; position:absolute; top:0; left:0;\" ng-disabled=\"hasFile\"><div class=\"base64-file--file-preview\" ng-show=\"hasFile\"><img ng-show=\"isImage(file)\" ng-src=\"{{file.src}}\" height=\"150\"><div class=\"base64-file--file\"><div class=\"base64-file--file-name\">{{file.name}}</div><div class=\"base64-file--file-size\">{{file.humanSize}}</div><div class=\"base64-file--file-remove\" ng-click=\"removeFile($event)\">&#10005</div></div></div><button type=\"button\" ng-hide=\"hasFile\"><div class=\"base64-file--drop-area-description\">{{form.placeholder || dropText}}</div></button></div><span class=\"help-block\" sf-message=\"form.description\"></span></div>");}]);
 angular.module('angularSchemaFormBase64FileUpload', [
-  'schemaForm',
-  'templates'
-]).provider('base64FileUploadConfig', function() {
-  this.setDropText = function (text) {
-    this.dropText = text;
-  };
-
-  this.$get = function () {
-    return this;
-  };
-}).config([
-  'schemaFormProvider',
-  'schemaFormDecoratorsProvider',
-  'sfBuilderProvider',
-  'sfPathProvider',
-  function(schemaFormProvider, schemaFormDecoratorsProvider, sfBuilderProvider, sfPathProvider) {
-
-    var base64file = function(name, schema, options) {
-      if (schema.type === 'string' && schema.format === 'base64') {
-        var f = schemaFormProvider.stdFormObj(name, schema, options);
-        f.key  = options.path;
-        f.type = 'base64file';
-        options.lookup[sfPathProvider.stringify(options.path)] = f;
-        return f;
-      }
+    'schemaForm',
+    'templates'
+]).provider('base64FileUploadConfig', function () {
+    this.setDropText = function (text) {
+        this.dropText = text;
     };
 
-    schemaFormProvider.defaults.string.unshift(base64file);
+    this.$get = function () {
+        return this;
+    };
+}).config([
+    'schemaFormProvider',
+    'schemaFormDecoratorsProvider',
+    'sfBuilderProvider',
+    'sfPathProvider',
+    function (schemaFormProvider, schemaFormDecoratorsProvider, sfBuilderProvider, sfPathProvider) {
 
-    schemaFormDecoratorsProvider.defineAddOn(
-      'bootstrapDecorator',           // Name of the decorator you want to add to.
-      'base64file',                      // Form type that should render this add-on
-      'src/templates/angular-schema-form-base64-file-upload.html',  // Template name in $templateCache
-      sfBuilderProvider.stdBuilders   // List of builder functions to apply.
-    );
-  }
+        var base64file = function (name, schema, options) {
+            if (schema.type === 'string' && schema.format === 'base64') {
+                var f = schemaFormProvider.stdFormObj(name, schema, options);
+                f.key = options.path;
+                f.type = 'base64file';
+                options.lookup[sfPathProvider.stringify(options.path)] = f;
+                return f;
+            }
+        };
+
+        schemaFormProvider.defaults.string.unshift(base64file);
+
+        schemaFormDecoratorsProvider.defineAddOn(
+            'bootstrapDecorator',           // Name of the decorator you want to add to.
+            'base64file',                      // Form type that should render this add-on
+            'src/templates/angular-schema-form-base64-file-upload.html',  // Template name in $templateCache
+            sfBuilderProvider.stdBuilders   // List of builder functions to apply.
+        );
+    }
 ]);
 
 angular.module('angularSchemaFormBase64FileUpload').directive('base64FileUpload', [
-  'base64FileUploadConfig',
-  '$timeout',
-  function(base64FileUploadConfig, $timeout) {
-    return {
-      restrict: 'A',
-      require: 'ngModel',
-      scope: true,
-      link: function(scope, element, attrs, ngModel) {
-        scope.ngModel = ngModel;
-        scope.dropAreaHover = false;
-        scope.file = undefined;
-        scope.fileError = false;
-        scope.dropText = base64FileUploadConfig.dropText || 'Click here or drop files to upload';
+    'base64FileUploadConfig',
+    '$timeout',
+    function (base64FileUploadConfig, $timeout) {
 
-        var validateFile = function(file) {
-          var valid = true;
-          var schema = scope.$eval(attrs.base64FileUpload).schema;
+        var imageExts = ["3ds", "bmp", "btif", "cgm", "cmx", "djv", "djvu", "dmg", "dwg", "dxf", "fbs", "fh", "fh4", "fh5", "fh7", "fhc", "fpx", "fst", "g3", "gif", "ico", "ief", "iso", "jng", "jpe", "jpeg", "jpg", "ktx", "mdi", "mmr", "npx", "odi", "oti", "pbm", "pct", "pcx", "pgm", "pic", "png", "pnm", "ppm", "psd", "ras", "rgb", "rlc", "sgi", "sid", "svg", "svgz", "t3", "tga", "tif", "tiff", "uvg", "uvi", "uvvg", "uvvi", "wbmp", "wdp", "webp", "xbm", "xif", "xpm", "xwd"]
 
-          if (file.size > parseInt(schema.maxSize, 10)) {
-            valid = false;
-            ngModel.$setValidity('base64FileUploadSize', false);
-          } else {
-            ngModel.$setValidity('base64FileUploadSize', true);
-          }
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            scope: true,
+            link: function (scope, element, attrs, ngModel) {
+                scope.ngModel = ngModel;
 
-          scope.$apply();
+                var baseName = function (str) {
+                    var base = new String(str).substring(str.lastIndexOf('/') + 1);
+                    console.log(base)
+                    if (base.lastIndexOf(".") != -1)
+                        base = base.substring(0, base.lastIndexOf("."));
+                    return base;
+                }
 
-          return valid;
-        }
+                scope.file = undefined;
 
-        var getFile = function(file) {
-          if (!file) {
-            return;
-          }
+                ngModel.$render = function () {
+                    var filename = ngModel.$viewValue || ''
+                    if (filename) {
+                        if (filename.indexOf(/^(http\:\/\/|https\:\/\/|\/)/igm) < 0)
+                            filename = '/' + filename
+                        scope.file = {
+                            ext: filename.split('.').slice(-1)[0],
+                            name: baseName(filename),
+                            src: filename
+                        }
+                        scope.hasFile = true
+                    }
+                }
 
-          if (!validateFile(file)) {
-            return;
-          }
+                scope.dropAreaHover = false;
+                scope.fileError = false;
+                scope.dropText = base64FileUploadConfig.dropText || 'Click here or drop files to upload';
 
-          var reader = new FileReader();
+                var validateFile = function (file) {
+                    var valid = true;
+                    var schema = scope.$eval(attrs.base64FileUpload).schema;
 
-          scope.file = file;
-          scope.file.ext = file.name.split('.').slice(-1)[0];
-          scope.file.src = URL.createObjectURL(file);
-          scope.hasFile = true;
-          // just a simple conversion to human readable size.
-          // For now not bothering with large sizes.
-          var fileSize = file.size / 1024;
-          var unit = 'kB';
-          if (fileSize > 1024) {
-            fileSize = fileSize / 1024;
-            unit = 'MB';
-          }
+                    if (file.size > parseInt(schema.maxSize, 10)) {
+                        valid = false;
+                        ngModel.$setValidity('base64FileUploadSize', false);
+                    } else {
+                        ngModel.$setValidity('base64FileUploadSize', true);
+                    }
 
-          scope.file.humanSize = fileSize.toFixed(1) + ' ' + unit;
+                    scope.$apply();
 
-          reader.onloadstart = function(e) {
-            $timeout(function() {
-              scope.loadingFile = true;
-            }, 0);
-          }
+                    return valid;
+                }
 
-          reader.onload = function(e) {
-            $timeout(function() {
-              scope.loadingFile = false;
-            }, 0);
-            
-            var prefix = 'file:' + file.name + ';';
-            ngModel.$setViewValue(prefix + e.target.result);
-          };
+                var getFile = function (file) {
+                    if (!file) {
+                        return;
+                    }
 
-          reader.readAsDataURL(file);
-          scope.$apply();
+                    if (!validateFile(file)) {
+                        return;
+                    }
+
+                    scope.file = file;
+                    scope.file.ext = file.name.split('.').slice(-1)[0];
+                    scope.file.src = URL.createObjectURL(file);
+                    scope.hasFile = true;
+                    // just a simple conversion to human readable size.
+                    // For now not bothering with large sizes.
+                    var fileSize = file.size / 1024;
+                    var unit = 'kB';
+                    if (fileSize > 1024) {
+                        fileSize = fileSize / 1024;
+                        unit = 'MB';
+                    }
+
+                    scope.file.humanSize = fileSize.toFixed(1) + ' ' + unit;
+
+                    var options = scope.$eval(attrs.base64FileUpload).schema.uploadOptions || {};
+
+                    if (options.endpoint) {
+                        var formData = new FormData();
+                        formData.append('file', file, file.name);
+                        for (var key in options) {
+                            if (key != 'endpoint')
+                                formData.append(key, options[key])
+                        }
+
+                        // Set up the request.
+                        var xhr = new XMLHttpRequest();
+
+                        // Open the connection.
+                        xhr.open('POST', options.endpoint, true);
+
+                        xhr.onload = function () {
+                            if (xhr.status === 200) {
+                                var data = JSON.parse(xhr.responseText)
+                                if (data.file)
+                                    ngModel.$setViewValue(data.file);
+                            } else {
+                                throw new Error(xhr.status + ': ' + xhr.statusText)
+                            }
+                        };
+
+                        // Send the Data.
+                        xhr.send(formData);
+
+                    } else {
+                        var reader = new FileReader();
+
+                        reader.onloadstart = function (e) {
+                            $timeout(function () {
+                                scope.loadingFile = true;
+                            }, 0);
+                        }
+
+                        reader.onload = function (e) {
+                            $timeout(function () {
+                                scope.loadingFile = false;
+                            }, 0);
+
+                            var prefix = 'file:' + file.name + ';';
+                            ngModel.$setViewValue(prefix + e.target.result);
+                        };
+
+                        reader.readAsDataURL(file);
+                    }
+
+                    scope.$apply();
+                };
+
+                scope.isImage = function (file) {
+                    if (!file) {
+                        return false;
+                    }
+
+                    return imageExts.indexOf(file.ext.toLowerCase()) > -1
+                }
+
+                scope.removeFile = function (e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    scope.file = undefined;
+                    scope.hasFile = false;
+                    ngModel.$setViewValue(undefined);
+                }
+
+                element.find('input').bind('change', function (e) {
+                    getFile(e.target.files[0]);
+                });
+
+                var dropArea = element[0];
+                var inputField = element.find('.base64-file--input')[0];
+
+                var clickArea = function (e) {
+                    e.stopPropagation();
+                    inputField.click();
+                }
+
+                var dragOver = function (e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    $timeout(function () {
+                        scope.dropAreaHover = true;
+                    }, 0);
+                };
+
+                var dragLeave = function (e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    $timeout(function () {
+                        scope.dropAreaHover = false;
+                    }, 0);
+                };
+
+                var drop = function (e) {
+                    dragLeave(e);
+                    getFile(e.dataTransfer.files[0]);
+                }
+
+                dropArea.addEventListener("click", clickArea, false);
+                dropArea.addEventListener("touchstart", clickArea, false);
+                dropArea.addEventListener("dragenter", dragOver, false);
+                dropArea.addEventListener("dragleave", dragLeave, false);
+                dropArea.addEventListener("dragover", dragOver, false);
+                dropArea.addEventListener("drop", drop, false);
+
+                scope.$on('$destroy', function () {
+                    dropArea.removeEventListener("click", clickArea, false);
+                    dropArea.removeEventListener("touchstart", clickArea, false);
+                    dropArea.removeEventListener("dragenter", dragOver, false);
+                    dropArea.removeEventListener("dragleave", dragLeave, false);
+                    dropArea.removeEventListener("dragover", dragOver, false);
+                    dropArea.removeEventListener("drop", drop, false);
+                });
+
+            },
         };
-
-        scope.isImage = function(file) {
-          if (!file) {
-            return false;
-          }
-
-          return file.type.indexOf('image') > -1
-        }
-
-        scope.removeFile = function(e) {
-          e.preventDefault();
-          e.stopPropagation();
-          scope.file = undefined;
-          scope.hasFile = false;
-          ngModel.$setViewValue(undefined);
-        }
-
-        element.find('input').bind('change', function(e) {
-          getFile(e.target.files[0]);
-        });
-
-        var dropArea = element.find('.base64-file--drop-area')[0];
-        var inputField = element.find('.base64-file--input')[0];
-
-        var clickArea = function(e) {
-          e.stopPropagation();
-          inputField.click();
-        }
-
-        var dragOver = function(e) {
-          e.preventDefault();
-          e.stopPropagation();
-          $timeout(function() {
-            scope.dropAreaHover = true;
-          }, 0);
-        };
-
-        var dragLeave = function(e) {
-          e.preventDefault();
-          e.stopPropagation();
-          $timeout(function() {
-            scope.dropAreaHover = false;
-          }, 0);
-        };
-
-        var drop = function(e) {
-          dragLeave(e);
-          getFile(e.dataTransfer.files[0]);
-        }
-
-        dropArea.addEventListener("click", clickArea, false);
-        dropArea.addEventListener("touchstart", clickArea, false);
-        dropArea.addEventListener("dragenter", dragOver, false);
-        dropArea.addEventListener("dragleave", dragLeave, false);
-        dropArea.addEventListener("dragover", dragOver, false);
-        dropArea.addEventListener("drop", drop, false);
-
-        scope.$on('$destroy', function () {
-          dropArea.removeEventListener("click", clickArea, false);
-          dropArea.removeEventListener("touchstart", clickArea, false);
-          dropArea.removeEventListener("dragenter", dragOver, false);
-          dropArea.removeEventListener("dragleave", dragLeave, false);
-          dropArea.removeEventListener("dragover", dragOver, false);
-          dropArea.removeEventListener("drop", drop, false);
-        });
-
-      },
-    };
-  }
+    }
 ]);
--- gulpfile.js
+++ gulpfile.js
@@ -6,38 +6,45 @@ var rename = require('gulp-rename');
 var templateCache = require('gulp-angular-templatecache');
 var streamqueue = require('streamqueue');
 var fs = require('fs');
+var minifyHtml = require('gulp-minify-html');
 
 gulp.task('default', ['minify', 'connect', 'watch']);
 
 gulp.task('connect', function () {
-  connect.server({
-    root: ['demo', './'],
-    livereload: true,
-  });
+    connect.server({
+        root: ['demo', './'],
+        livereload: true,
+    });
 });
 
 gulp.task('reload', ['minify'], function () {
-  gulp.src('./dist/**/*.*').pipe(connect.reload());
+    gulp.src('./dist/**/*.*').pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['./src/**', './demo/**'], ['reload']);
+    gulp.watch(['./src/**', './demo/**'], ['reload']);
 });
 
 gulp.task('minify', function () {
-  var files = JSON.parse(fs.readFileSync('sources.json', 'utf-8'));
-  var stream = streamqueue({ objectMode: true },
-    gulp.src(['src/templates/**/*.html']).pipe(templateCache({
-      standalone: true,
-      root: 'src/templates/',
-    })),
-    gulp.src(files)
-  )
-  .pipe(concat('angular-schema-form-base64-file-upload.js'))
-  .pipe(gulp.dest('./dist'))
-  .pipe(uglify())
-  .pipe(rename('angular-schema-form-base64-file-upload.min.js'))
-  .pipe(gulp.dest('./dist'));
+    var files = JSON.parse(fs.readFileSync('sources.json', 'utf-8'));
+    var stream = streamqueue({ objectMode: true },
+        gulp.src(['src/templates/**/*.html'])
+            .pipe(minifyHtml({
+                empty: true,
+                spare: true,
+                quotes: true
+            }))
+            .pipe(templateCache({
+                standalone: true,
+                root: 'src/templates/',
+            })),
+        gulp.src(files)
+    )
+        .pipe(concat('angular-schema-form-base64-file-upload.js'))
+        .pipe(gulp.dest('./dist'))
+        .pipe(uglify())
+        .pipe(rename('angular-schema-form-base64-file-upload.min.js'))
+        .pipe(gulp.dest('./dist'));
 
-  return stream;
+    return stream;
 });
